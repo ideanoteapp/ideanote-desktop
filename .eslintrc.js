df075468159f3a86fac04f3691828d90eba3515e
Korange <hi@korange.work>
2023-11-05 11:11:31 +0900

    :art: Clean codes with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard',
-    'plugin:vue/vue3-essential',
-    'prettier'
-  ],
+  extends: ["standard", "plugin:vue/vue3-essential", "prettier"],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'vue'
-  ],
-  rules: {
-  }
-}
+  plugins: ["vue"],
+  rules: {},
+};
