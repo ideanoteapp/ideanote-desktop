bf33133ab82bdfc333f7ed959aa4b34669770a7b
Korange <hi@korange.work>
2023-11-12 09:04:04 +0900

    :art: Clean Codes with Prittier

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -3,18 +3,26 @@
 
   <div class="h-[calc(100%-50px)] flex flex-col">
     <div class="">
-      <div
-        v-for="i in list"
-        class="flex rounded-lg text-white px-4 py-1"
-      >
+      <div v-for="i in list" class="flex rounded-lg text-white px-4 py-1">
         <div class="flex-grow text-white flex">
-          <input type="checkbox" name="" id="" @change="save" v-model="i.checked" class="w-6 h-6">
-           <div class="ml-3 flex flex-grow flex-col justify-center text">
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            @change="save"
+            v-model="i.checked"
+            class="w-6 h-6"
+          />
+          <div class="ml-3 flex flex-grow flex-col justify-center text">
             {{ i.text }}
-           </div>
-           <button @click="remove(i.text)"><font-awesome-icon icon="fa-solid fa-trash" class="text-[#4d4c4c] duration-100 hover:text-[#ff6262] mt-1 text-[1rem]" /></button>
           </div>
-        
+          <button @click="remove(i.text)">
+            <font-awesome-icon
+              icon="fa-solid fa-trash"
+              class="text-[#4d4c4c] duration-100 hover:text-[#ff6262] mt-1 text-[1rem]"
+            />
+          </button>
+        </div>
       </div>
     </div>
     <form
@@ -26,7 +34,7 @@
         class="rounded-lg text-white flex-grow p-2 bg-transparent"
         style="outline: none !important; resize: none; caret-color: white"
         placeholder="Type something..."
-      >
+      />
       <button
         type="submit"
         class="rounded-lg w-10 h-10 flex justify-center flex-col text-center bg-[#2F497D]"
@@ -95,17 +103,17 @@ export default {
   },
   methods: {
     addText() {
-      this.list.push({checked: false, text: this.input});
+      this.list.push({ checked: false, text: this.input });
       this.input = "";
       this.$emit("save", JSON.stringify(this.list));
     },
-    save(){
-      this.$emit("save", JSON.stringify(this.list))
+    save() {
+      this.$emit("save", JSON.stringify(this.list));
     },
-    remove(text){
-      this.list = this.list.filter(item => item.text !== text);
-      this.$emit("save", JSON.stringify(this.list))
-    }
+    remove(text) {
+      this.list = this.list.filter((item) => item.text !== text);
+      this.$emit("save", JSON.stringify(this.list));
+    },
   },
 };
 </script>
