a312f15c6ea134054892360802bead10ecd733aa
Korange <hi@korange.work>
2023-11-30 13:46:07 +0900

    v1.7.2

--- main.js
+++ main.js
@@ -508,7 +508,7 @@ app.whenReady().then(() => {
   createWindow();
 
   if(AutoUpdate){
-    const currentVersion = "1.7.1";
+    const currentVersion = "1.7.2";
     axios
     .get("https://ideanote-updates.korange.work/info.json", {})
     .then((response) => {
