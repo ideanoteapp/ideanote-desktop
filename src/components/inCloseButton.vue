40607df4ddb305cbeecdd36e73a266825de4c7f8
Korange <hi@korange.work>
2024-02-08 17:48:36 +0900

    Componentization Buttons

---
+++ src/components/inCloseButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button
+      class="fixed top-4 z-50 right-4"
+    >
+      <div
+        class="w-9 h-9 text-center flex justify-center fles-col text-white bg-[#b93232] rounded-lg"
+      >
+        <div class="flex flex-col justify-center">
+          <font-awesome-icon icon="fa-solid fa-xmark" />
+        </div>
+      </div>
+    </button>
+</template>
